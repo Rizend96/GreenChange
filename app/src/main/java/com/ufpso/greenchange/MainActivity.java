3556edb9bf3c868e04299841aec8cf9614fe068d
Oscar Argaez <rizend96@gmail.com>
2021-11-30 15:53:38 -0500

    init commit

---
+++ app/src/main/java/com/ufpso/greenchange/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ufpso.greenchange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
